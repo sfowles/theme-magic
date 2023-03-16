ccfef322a42591a82168b3337c3ea2892476b3e0
Scott Fowles <sfowles@gmail.com>
2023-03-16 08:33:10 -0700

    Add files via upload

---
+++ code.ts
@@ -0,0 +1,400 @@
+// Map tokens from Light to Dark theme
+const lightTokens:any = {
+  // semantic/light/primary action
+  "S:839e704ead3c98ea1ec04013b09ec2b1d465865f,399:3": {
+    mapsTo: "S:172f22137f95de7d37c8eb8544977af26634833b,324:20" // dark mode variant
+  },
+  // semantic/light/primary action hover
+  "S:ba2886363a43d70c667403e6faf8da2e548c32d7,324:17": {
+    mapsTo: "S:b5acd2524c0cbb8d1816b968ae04826b968947d5,324:21"
+  },
+  // semantic/light/secondary action
+  "S:477f01f197730d3ae6bab79acaf3160b549765b2,324:18": {
+    mapsTo: "S:e8ed423a4abbc06a342880a0b8337d8f9aef3f16,324:22"
+  },
+  // semantic/light/secondary action hover
+  "S:1edfcc358de554aee032afc771ec925316d56682,324:19": {
+    mapsTo: "S:7bb465ab5b217b23a936887d8f0d0807506ccef8,324:23"
+  },
+  // semantic/light/selected
+  "S:034934443ff4577d9a66dcdc50579ad1fde8e4f4,500:0": {
+    mapsTo: "S:5628f22fd895eb6289b5e4fd5e2056348f6eefee,500:1"
+  },
+  // semantic/light/container
+  "S:e788850555bd2a31c32990473519d9e20398d75c,182:20": {
+    mapsTo: "S:ab32036af359f8b8ed8fea35ad875f9e25771c97,182:21"
+  },
+  // semantic/light/background
+  "S:d4caeb649b37339a8fefe235a401e2eac93a5418,167:103": {
+    mapsTo: "S:10e3da407a9bfe44d4ab45d7c64f3b1916d05611,112:175"
+  },
+  // semantic/light/disabled input background
+  "S:d1993e63e0b362adcddc07947d26a590e0e002d9,368:15": {
+    mapsTo: "S:aa66bcd2637da20b09011b322b9427f0e906744d,369:18"
+  },
+  // semantic/light/input background
+  "S:c91db3a7bd1072c42983d379d7472162acf6fbad,182:23": {
+    mapsTo: "S:c91db3a7bd1072c42983d379d7472162acf6fbad,182:23"
+  },
+  // semantic/light/navigation background
+  "S:e54cd10b9a272aa703c188861da21fb250881785,367:6": {
+    mapsTo: "S:83735442c8bef259bc9753378bb0e2d6d6a772f3,367:9"
+  },
+  // semantic/light/surface
+  "S:a2230c3e8605f8253eb05339e3df58aa32c90cde,167:102": {
+    mapsTo: "S:bdea11d3867117a7dbddf07b79049cde84d3d486,114:369"
+  },
+  // semantic/light/divider
+  "S:af7285aadc249144801ff30ac1881c4fdd36e78e,369:27": {
+    mapsTo: "S:c575457f7eb850951675d2186c5c828219eff260,369:24"
+  },
+  // semantic/light/disabled background
+  "S:51e7a9ca736db46aec25ec2f2ea593bb968c3e89,182:28": {
+    mapsTo: "S:df3bca4987fadca9198208807dcd0f7a1e057459,182:29"
+  },
+  // semantic/light/icon
+  "S:fe9a904fafb5dcb1e161d4b3a7eb0f8e7f21d3ce,161:18": {
+    mapsTo: "S:c64abe6fd217f1640bcef298dfcd862b28968502,161:21"
+  },
+  // semantic/light/success background
+  "S:f2cb35bab91f16e912f17ce08bbc21ff95387a4a,381:30": {
+    mapsTo: "S:0503911db4b713d5e777c61b46dad91a4b6bdf29,381:36"
+  },
+  // semantic/light/success
+  "S:07f371a4c8010fd695462502016de5e977b59d3d,201:0": {
+    mapsTo: "S:8ebfa3ede1626167f09c1a53f239e157998a50aa,112:187"
+  },
+  // semantic/light/success hover
+  "S:430aa24d74b232a4fb241a57da83b01ee4771a15,201:1": {
+    mapsTo: "S:2582839954f320ba75058bb442829c3190cba314,182:33"
+  },
+  // semantic/light/warning
+  "S:3fe847d123e7006d772b525416e5536f9f29d0b4,112:162": {
+    mapsTo: "S:261e22c36f9fa4f506d3cc9ab04fdfc5c65437ec,112:193"
+  },
+  // semantic/light/warning hover
+  "S:8f51bea2d886c262d7b340a480f777f03379592a,182:30": {
+    mapsTo: "S:a1a946d389b2f0038122b3d8e3c9f2616a8a7493,182:34"
+  },
+  // semantic/light/danger background
+  "S:2241d699a606af09ad67bfcab422f8211939d510,381:33": {
+    mapsTo: "S:3cd521741a83faff5ee9ced163ff21ea8f736656,381:39"
+  },
+  // semantic/light/danger
+  "S:5542b111aaf2f245d8465d8f2bdaf3358d12c65a,112:366": {
+    mapsTo: "S:7f9138df9b58732cda790fd16f1c50445dc07947,112:199"
+  },
+  // semantic/light/danger hover
+  "S:1339e185ee62a1498f0c70a60e6f1975325370fb,182:32": {
+    mapsTo: "S:f6ffe82ed89c33a4ad0133f11049da0014264c4a,182:35"
+  },
+  // semantic/light/text/high contrast
+  "S:2053987dd3a5194df8ca0e13fce89a521298ac96,127:24": {
+    mapsTo: "S:aa860e7dc039e0c409073ff761aec3ec08454873,112:205"
+  },
+  // semantic/light/text/medium contrast
+  "S:91cd0c517e09c329affb846b90c8b94a38c73f5e,112:147": {
+    mapsTo: "S:bae987d9c1e5c41515b4333b2f01c4eca4251720,112:208"
+  },
+  // semantic/light/text/low contrast
+  "S:977a44ee812c4569ea22d2d75e28742fd98597ca,112:146": {
+    mapsTo: "S:5605c64dd8f62160b58ee22486418c11dc45808b,112:211"
+  },
+  // semantic/light/text/disabled
+  "S:058b48aa02f6f3b8474f440da8845751fca80362,416:430": {
+    mapsTo: "S:55a90b23bddd3c9f8f76ac2f52784d3e42ad0e56,416:429"
+  },
+  // semantic/light/text/primary action
+  "S:b3ba3e1bdc76413955cf14f4025cd9ae9a51098c,310:14": {
+    mapsTo: "S:2ca2ec62b6a52acc8bb03af85633ef86fdfa2ec9,310:15"
+  },
+  // semantic/light/text/navigation
+  "S:32171a5063e5261c23bae2b979835ec2f55bbcb8,367:2": {
+    mapsTo: "S:cc1becf5f81f93aeeaf3e5e1b36b3b191bb131be,367:12"
+  },
+  // semantic/light/text/link
+  "S:8d6492c6d5047e3cf4358c07ba0a7f74c1b41db6,182:10": {
+    mapsTo: "S:2d31ceb80b0caf347354525ef4ec790db56be10c,182:16"
+  },
+  // semantic/light/text/link hover
+  "S:f3a26bd9ba8104516d425d4aba255872d72b6273,182:13": {
+    mapsTo: "S:b03fab19b1d74286d412d310796f61c5612a7275,182:19"
+  }
+}
+
+// Map tokens from Dark to Light theme
+const darkTokens:any = {
+  // semantic/dark/primary action
+  "S:172f22137f95de7d37c8eb8544977af26634833b,324:20": {
+    mapsTo: "S:839e704ead3c98ea1ec04013b09ec2b1d465865f,399:3" // light mode variant
+  },
+  // semantic/dark/primary action hover
+  "S:b5acd2524c0cbb8d1816b968ae04826b968947d5,324:21": {
+    mapsTo: "S:ba2886363a43d70c667403e6faf8da2e548c32d7,324:17"
+  },
+  // semantic/dark/secondary action
+  "S:e8ed423a4abbc06a342880a0b8337d8f9aef3f16,324:22": {
+    mapsTo: "S:477f01f197730d3ae6bab79acaf3160b549765b2,324:18"
+  },
+  // semantic/dark/secondary action hover
+  "S:7bb465ab5b217b23a936887d8f0d0807506ccef8,324:23": {
+    mapsTo: "S:1edfcc358de554aee032afc771ec925316d56682,324:19"
+  },
+  // semantic/dark/selected
+  "S:5628f22fd895eb6289b5e4fd5e2056348f6eefee,500:1": {
+    mapsTo: "S:034934443ff4577d9a66dcdc50579ad1fde8e4f4,500:0"
+  },
+  // semantic/dark/container
+  "S:ab32036af359f8b8ed8fea35ad875f9e25771c97,182:21": {
+    mapsTo: "S:e788850555bd2a31c32990473519d9e20398d75c,182:20"
+  },
+  // semantic/dark/background
+  "S:10e3da407a9bfe44d4ab45d7c64f3b1916d05611,112:175": {
+    mapsTo: "S:d4caeb649b37339a8fefe235a401e2eac93a5418,167:103"
+  },
+  // semantic/dark/disabled input background
+  "S:aa66bcd2637da20b09011b322b9427f0e906744d,369:18": {
+    mapsTo: "S:d1993e63e0b362adcddc07947d26a590e0e002d9,368:15"
+  },
+  // semantic/dark/input background
+  "S:c91db3a7bd1072c42983d379d7472162acf6fbad,182:23": {
+    mapsTo: "S:c91db3a7bd1072c42983d379d7472162acf6fbad,182:23"
+  },
+  // semantic/dark/navigation background
+  "S:83735442c8bef259bc9753378bb0e2d6d6a772f3,367:9": {
+    mapsTo: "S:e54cd10b9a272aa703c188861da21fb250881785,367:6"
+  },
+  // semantic/dark/surface
+  "S:bdea11d3867117a7dbddf07b79049cde84d3d486,114:369": {
+    mapsTo: "S:a2230c3e8605f8253eb05339e3df58aa32c90cde,167:102"
+  },
+  // semantic/dark/divider
+  "S:c575457f7eb850951675d2186c5c828219eff260,369:24": {
+    mapsTo: "S:af7285aadc249144801ff30ac1881c4fdd36e78e,369:27"
+  },
+  // semantic/dark/disabled background
+  "S:df3bca4987fadca9198208807dcd0f7a1e057459,182:29": {
+    mapsTo: "S:51e7a9ca736db46aec25ec2f2ea593bb968c3e89,182:28"
+  },
+  // semantic/dark/icon
+  "S:c64abe6fd217f1640bcef298dfcd862b28968502,161:21": {
+    mapsTo: "S:fe9a904fafb5dcb1e161d4b3a7eb0f8e7f21d3ce,161:18"
+  },
+  // semantic/dark/success background
+  "S:0503911db4b713d5e777c61b46dad91a4b6bdf29,381:36": {
+    mapsTo: "S:f2cb35bab91f16e912f17ce08bbc21ff95387a4a,381:30"
+  },
+  // semantic/dark/success
+  "S:8ebfa3ede1626167f09c1a53f239e157998a50aa,112:187": {
+    mapsTo: "S:07f371a4c8010fd695462502016de5e977b59d3d,201:0"
+  },
+  // semantic/dark/success hover
+  "S:2582839954f320ba75058bb442829c3190cba314,182:33": {
+    mapsTo: "S:430aa24d74b232a4fb241a57da83b01ee4771a15,201:1"
+  },
+  // semantic/dark/warning
+  "S:261e22c36f9fa4f506d3cc9ab04fdfc5c65437ec,112:193": {
+    mapsTo: "S:3fe847d123e7006d772b525416e5536f9f29d0b4,112:162"
+  },
+  // semantic/dark/warning hover
+  "S:a1a946d389b2f0038122b3d8e3c9f2616a8a7493,182:34": {
+    mapsTo: "S:8f51bea2d886c262d7b340a480f777f03379592a,182:30"
+  },
+  // semantic/dark/danger background
+  "S:3cd521741a83faff5ee9ced163ff21ea8f736656,381:39": {
+    mapsTo: "S:2241d699a606af09ad67bfcab422f8211939d510,381:33"
+  },
+  // semantic/dark/danger
+  "S:7f9138df9b58732cda790fd16f1c50445dc07947,112:199": {
+    mapsTo: "S:5542b111aaf2f245d8465d8f2bdaf3358d12c65a,112:366"
+  },
+  // semantic/dark/danger hover
+  "S:f6ffe82ed89c33a4ad0133f11049da0014264c4a,182:35": {
+    mapsTo: "S:1339e185ee62a1498f0c70a60e6f1975325370fb,182:32"
+  },
+  // semantic/dark/text/high contrast
+  "S:aa860e7dc039e0c409073ff761aec3ec08454873,112:205": {
+    mapsTo: "S:2053987dd3a5194df8ca0e13fce89a521298ac96,127:24"
+  },
+  // semantic/dark/text/medium contrast
+  "S:bae987d9c1e5c41515b4333b2f01c4eca4251720,112:208": {
+    mapsTo: "S:91cd0c517e09c329affb846b90c8b94a38c73f5e,112:147"
+  },
+  // semantic/dark/text/low contrast
+  "S:5605c64dd8f62160b58ee22486418c11dc45808b,112:211": {
+    mapsTo: "S:977a44ee812c4569ea22d2d75e28742fd98597ca,112:146"
+  },
+  // semantic/dark/text/disabled
+  "S:55a90b23bddd3c9f8f76ac2f52784d3e42ad0e56,416:429": {
+    mapsTo: "S:058b48aa02f6f3b8474f440da8845751fca80362,416:430"
+  },
+  // semantic/dark/text/primary action
+  "S:2ca2ec62b6a52acc8bb03af85633ef86fdfa2ec9,310:15": {
+    mapsTo: "S:b3ba3e1bdc76413955cf14f4025cd9ae9a51098c,310:14"
+  },
+  // semantic/dark/text/navigation
+  "S:cc1becf5f81f93aeeaf3e5e1b36b3b191bb131be,367:12": {
+    mapsTo: "S:32171a5063e5261c23bae2b979835ec2f55bbcb8,367:2"
+  },
+  // semantic/dark/text/link
+  "S:2d31ceb80b0caf347354525ef4ec790db56be10c,182:16": {
+    mapsTo: "S:8d6492c6d5047e3cf4358c07ba0a7f74c1b41db6,182:10"
+  },
+  // semantic/dark/text/link hover
+  "S:b03fab19b1d74286d412d310796f61c5612a7275,182:19": {
+    mapsTo: "S:f3a26bd9ba8104516d425d4aba255872d72b6273,182:13"
+  }
+}
+
+// Define selected Frame(s)
+const selectedFrames = figma.currentPage.selection;
+
+// Skip invisible stuff
+figma.skipInvisibleInstanceChildren = true;
+
+// Die if no Frame is selected
+if (!selectedFrames[0]) {
+  figma.notify("Select one or more Frames to experience the magic! ⚠️");
+  figma.closePlugin();
+} else {
+
+  // Die if things other than Frames are selected
+  selectedFrames.forEach(selectedFrame => {
+    if (selectedFrame.parent && selectedFrame.parent.type === 'PAGE' && selectedFrame.type === 'FRAME') {
+
+      // Clone selected Frame
+      selectedFrame.clone();
+
+      // Find all Frames in the current page
+      const pageChildren = figma.currentPage.children;
+
+      // Iterate over all page children to find x position for each
+      let largest = 0;
+      const y = selectedFrame.y;
+
+      pageChildren.forEach(child => {
+        if (child.y === y && largest < child.x) {
+          largest = child.x;
+        }
+      });
+
+      // Move clone 40px to the right of the last child in the same row
+      selectedFrame.x = largest + selectedFrame.width + 40;
+
+      // Find all instances within clone
+      const instances = selectedFrame.findAll((n: { type: string; }) => n.type === "INSTANCE");
+      let fillStyle = selectedFrame.fillStyleId;
+      let strokeStyle;
+      let currentTheme = '';
+
+      // Check current theme and set currentTheme value
+      instances.every(instance => {
+        if (instance.type === 'INSTANCE') {
+          let name = selectedFrame.name;
+          let newName;
+          if ('Theme' in instance.componentProperties
+            && instance.componentProperties.Theme.value === 'Light') {
+              currentTheme = 'light';
+              if (name.match(/light/i)) {
+                newName = name.replace(/light/gi, "Dark");
+              } else {
+                newName = name + ' / Dark';
+              }
+              selectedFrame.name = newName;
+              return false;
+          }
+          if ('Theme' in instance.componentProperties
+            && instance.componentProperties.Theme.value === 'Dark') {
+              currentTheme = 'dark';
+              if (name.match(/dark/i)) {
+                newName = name.replace(/dark/gi, "Light");
+              } else {
+                newName = name + ' / Light';
+              }
+              selectedFrame.name = newName;
+              return false;
+          }
+          return true;
+        }
+      });
+
+      // Recursive function to traverse all child nodes
+      const traverse = (e: any) => {
+        if (e.type === 'INSTANCE') {
+          if ('Theme' in e.componentProperties && currentTheme === 'light') {
+            e.setProperties({ 'Theme': 'Dark' });
+          }
+          if ('Theme' in e.componentProperties && currentTheme === 'dark') {
+            e.setProperties({ 'Theme': 'Light' });
+          }
+        }
+
+        if (e.type !== 'INSTANCE') {
+          fillStyle = e.fillStyleId;
+          strokeStyle = e.strokeStyleId;
+          if (currentTheme === 'light' && fillStyle !== '') {
+            for (const light in lightTokens) {
+              if (light === fillStyle) {
+                e.fillStyleId = lightTokens[light].mapsTo;
+                break;
+              }
+            }
+          }
+          if (currentTheme === 'light' && strokeStyle !== '') {
+            for (const light in lightTokens) {
+              if (light === strokeStyle) {
+                e.strokeStyleId = lightTokens[light].mapsTo;
+                break;
+              }
+            }
+          }
+          if (currentTheme === 'dark' && fillStyle !== '') {
+            for (const dark in darkTokens) {
+              if (dark === fillStyle) {
+                e.fillStyleId = darkTokens[dark].mapsTo;
+                break;
+              }
+            }
+          }
+          if (currentTheme === 'dark' && strokeStyle !== '') {
+            for (const dark in darkTokens) {
+              if (dark === strokeStyle) {
+                e.strokeStyleId = darkTokens[dark].mapsTo;
+                break;
+              }
+            }
+          }
+        }
+
+        if (e.type === 'GROUP'
+        || e.type === 'FRAME') {
+          for (const child in e.children) {
+            traverse(e.children[child]);
+          }
+        }
+      }
+
+      // Check for and replace fill color on the selected Frame
+      traverse(selectedFrame);
+
+      // Iterate through children of selected Frame
+      for (const child in selectedFrame.children) {
+        traverse(selectedFrame.children[child]);
+      }
+
+      figma.viewport.scrollAndZoomIntoView(selectedFrames);
+
+      let run = async () => {
+        // Success notice and close plugin
+        figma.notify("Theme magic! ✨");
+        figma.closePlugin();
+      }
+      
+      run();
+    } else {
+      figma.notify("Invalid selection! The magic only works on page level Frames. ⚠️");
+      figma.closePlugin();
+    }
+  });
+
+}
