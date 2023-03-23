d3f09b13181f6c06f4d711a3e1e6c61dbe276a13
sfowles <sfowles@gmail.com>
2023-03-22 18:57:37 -0700

    Move source files into /src

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = (env, argv) => ({
   devtool: argv.mode === 'production' ? false : 'inline-source-map',
 
   entry: {
-    code: './code.ts', // The entry point for your plugin code
+    code: './src/code.ts', // The entry point for your plugin code
   },
 
   module: {
